ab39fb8cba044fb296dcc8698e87c7858ce61172
gowthamreddy4561 <113674970+gowthamreddy4561@users.noreply.github.com>
2024-04-04 15:53:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
-import {fileURLToPath} from "url";
+
 
 
 
@@ -30,8 +30,11 @@ const __dirname=path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,"./client/build")))
-app.use("*",function(req,res){
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+//static files
+
+app.use(express.static(path.join(__dirname,"./client/build")));
+app.get('*', function (req,res) {
   res.sendFile(path.join(__dirname,"./client/build/index.html"))
 })
 
